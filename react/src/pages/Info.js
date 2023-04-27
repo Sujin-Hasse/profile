ec5f9f2a2c58021eeaf5c0cd9d5eba819d40113f
Hasse <2017104858@khu.ac.kr>
2023-04-27 15:30:03 +0900

    feat: info coomponent

--- react/src/pages/Info.js
+++ react/src/pages/Info.js
@@ -1,28 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const Info = (props) => {
-  const { name, major, mbti, birth } = props.data;
+const Info = () => {
   return (
     <>
-      <h3>{name}</h3>
-      <div class="profile-contents">
-        <div class="birth">
-          <p>Birth</p>
-          <p>{birth}</p>
-        </div>
-        <div class="major">
-          <p>Major</p>
-          <p>{major}</p>
-        </div>
-        <div class="mbti">
-          <p>MBTI</p>
-          <p>{mbti}</p>
-        </div>
-        <div class="social-media">
-          <p>Social Media</p>
-          <p>{socialmedia}</p>
-        </div>
-      </div>
+      <div class="info">
+  <div class="catch-praise">
+    <div class="title">
+      <h2><span class="pink">새로움과 도전</span>이 가득한<br>라이프를 추구합니다</h2></div>
+    <div class="subtitle">
+      <p>스포츠와 IT Product로 풍부해지는 하쎄월드에 오신 여러분을 환영합니다!<br>
+          에너지 넘치는 저와 함께, 실력향상도, 코딩도, 프로젝트도 모두 멋지게 완수하는 2023년 보내보아요 :-)</p></div>
+  </div>
     </>
   );
 };
